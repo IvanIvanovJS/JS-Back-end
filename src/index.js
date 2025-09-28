76b1117fb18deec8669697c35fb6fecd9d75757a
IvanIvanovJS <sibsmartco@gmail.com>
2025-09-28 16:37:08 +0300

    added main layout and about page

---
+++ src/index.js
@@ -0,0 +1,27 @@
+import express from "express";
+import handlebars from "express-handlebars";
+
+const app = express();
+
+app.engine("hbs", handlebars.engine(
+    {
+        extname: "hbs"
+    }
+));
+
+app.set('view engine', 'hbs');
+app.set('views', 'src/views')
+
+app.use(express.static('src/public'))
+
+app.get('/', (req, res) => {
+    res.render('home')
+});
+
+app.get('/about', (req, res) => {
+    res.render('about')
+})
+
+app.listen(5000, () => {
+    console.log('Server is listening on port http://localhost:5000...')
+})
