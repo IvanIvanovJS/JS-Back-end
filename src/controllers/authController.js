3461d6b8ad39b698b81a85fe6000b6aa668436be
IvanIvanovJS <sibsmartco@gmail.com>
2025-10-02 10:20:44 +0300

    save user to db

--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -1,9 +1,15 @@
 import { Router } from "express";
+import userSurice from "../services/userSurice.js";
 
 const authController = Router();
 
 authController.get('/register', (req, res) => {
     res.render('auth/register')
+})
+
+authController.post('/register', async (req, res) => {
+    const userData = req.body
+    await userSurice.register(userData)
 })
 
 export default authController;
