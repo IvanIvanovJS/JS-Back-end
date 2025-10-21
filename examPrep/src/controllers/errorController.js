2c93094fe0834953c06c1cbcab13306ce2176518
IvanIvanovJS <sibsmartco@gmail.com>
2025-10-21 10:53:21 +0300

    added handlebars engine

--- examPrep/src/controllers/errorController.js
+++ examPrep/src/controllers/errorController.js
@@ -3,6 +3,7 @@ import { Router } from "express";
 const errorController = Router()
 
 errorController.all('/*path', (req, res) => {
+
     res.send('404 Not found!')
 })
 
--- examPrep/src/index.js
+++ examPrep/src/index.js
@@ -1,7 +1,12 @@
 import express from "express";
 import routes from "./routes.js";
-
+import handlebars from "express-handlebars"
 const app = express();
+
+app.engine('hbs', handlebars.engine({
+    extname: 'hbs'
+}))
+app.set('view engine', 'hbs')
 
 app.use(express.static('src/public'))
 app.use(express.urlencoded({ extended: false }))
